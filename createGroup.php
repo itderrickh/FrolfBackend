1fa0a56234c852976354a3e0a796fc0012ac0f50
itderrickh <heined50@uwosh.edu>
2016-09-25 20:22:25 -0500

    Added long/lat to groups

--- createGroup.php
+++ createGroup.php
@@ -5,10 +5,12 @@ require_once './verify.php';
 
 $groupDAO = new GroupDAO($config);
 $groupName = $_POST["groupName"];
+$latitude = $_POST["latitude"];
+$longitude = $_POST["longitude"];
 $token = $_SERVER['HTTP_AUTHORIZE'];
 
 if(verifyToken($token)) {
-    $groupDAO->createGroup($groupName);
+    $groupDAO->createGroup($groupName, $latitude, $longitude);
 } else {
     header('HTTP/1.1 401 Unauthorized');
 }
--- dbclasses/group.php
+++ dbclasses/group.php
@@ -5,17 +5,17 @@ class GroupDAO {
         $this->config = $config;
     }
     
-    function createGroup($userId) {
+    function createGroup($userId, $latitude, $longitude) {
         $mysqli = new mysqli($this->config['dbhost'], $this->config['dbuser'], $this->config['dbpass'], $this->config['dbdatabase']);
-        $stmt = $mysqli->prepare("INSERT INTO groups(name, datecreated) VALUES (?, ?)");
+        $stmt = $mysqli->prepare("INSERT INTO groups(name, datecreated) VALUES (?, ?, ?, ?)");
         $createDate = date("Y-m-d");
         $stmt->bind_param("sd", $name, $createDate);
         $stmt->execute();
         $stmt->close();
         
         $groupId = $mysqli->insert_id;
-        $stmt2 = $mysqli->prepare("INSERT INTO usergroups(groupid, userid) VALUES (?, ?, 1)");
-        $stmt2->bind_param("ii", $groupId, $userId);
+        $stmt2 = $mysqli->prepare("INSERT INTO usergroups(groupid, userid, latitude, longitude) VALUES (?, ?, 1)");
+        $stmt2->bind_param("iidd", $groupId, $userId, $latitude, $longitude);
         $stmt2->execute();
         $stmt2->close();
         
