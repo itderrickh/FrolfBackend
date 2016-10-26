83bfbc331ff5a8aae1a7903d9d48b95dfc91b143
itderrickh <heined50@uwosh.edu>
2016-10-25 23:52:12 -0500

    Added api defs to get and add friends.

--- dbclasses/friends.php
+++ dbclasses/friends.php
@@ -27,5 +27,37 @@ class FriendsDAO {
 
         return $resultGroups;
     }
+
+    function addFriend($userId, $friendId) {
+        $mysqli = new mysqli($this->config['dbhost'], $this->config['dbuser'], $this->config['dbpass'], $this->config['dbdatabase']);
+        $stmt = $mysqli->prepare("INSERT INTO friends (dateadded, friendid, userid) VALUES (NOW(), ?, ?)");
+        $stmt->bind_param("i", $friendId, $userId);
+        $stmt->execute();
+
+        $stmt->close();
+        $mysqli->close();
+    }
+
+    function getFriends($userId) {
+        $mysqli = new mysqli($this->config['dbhost'], $this->config['dbuser'], $this->config['dbpass'], $this->config['dbdatabase']);
+        $stmt = $mysqli->prepare("SELECT friendUser.email, friends.dateadded FROM friends
+                                  LEFT JOIN users AS friendUser ON friendUser.id = friendid
+                                  WHERE friends.userid = ?");
+        $stmt->bind_param("i", $userId);
+        $stmt->execute();
+
+        $resultGroups = array();
+        $stmt->bind_result($id, $email);
+        while ($stmt->fetch()) {
+            $row['id'] = $id;
+            $row['email'] = $email;
+            array_push($resultGroups, $row);
+        }
+
+        $stmt->close();
+        $mysqli->close();
+
+        return $resultGroups;
+    }
 }
 ?>
---
+++ getFriends.php
@@ -0,0 +1,21 @@
+<?php
+require_once './config.php';
+require_once './dbclasses/friends.php';
+require_once './dbclasses/user.php';
+require_once './verify.php';
+
+$AJAX_FORM = json_decode(file_get_contents('php://input'), true);
+
+$friendsDAO = new FriendsDAO($config);
+$userDAO = new UserDAO($config);
+$token = $_SERVER['HTTP_AUTHORIZE'];
+
+if(verifyToken($token, $config)) {
+    $tokenInfo = getTokenInfo($token);
+    $user = $userDAO->getUser($tokenInfo['email']);
+    $friends = $friendsDAO->getFriends($user['id']);
+    echo json_encode($friends);
+} else {
+    header('HTTP/1.1 401 Unauthorized');
+}
+?>
