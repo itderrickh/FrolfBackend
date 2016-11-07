1701c8fc36f12bf715f1a8027d4d656e6be49238
itderrickh <heined50@uwosh.edu>
2016-11-07 15:06:24 -0600

    Added user image part

---
+++ dbclasses/userimages.php
@@ -0,0 +1,52 @@
+<?php
+class UserImagesDAO {
+    private $config;
+    function __construct($config) {
+        $this->config = $config;
+    }
+
+    function doesImageExist($userId) {
+        $mysqli = new mysqli($this->config['dbhost'], $this->config['dbuser'], $this->config['dbpass'], $this->config['dbdatabase']);
+        $stmt = $mysqli->prepare("SELECT COUNT(*) FROM userimages WHERE userid = ?");
+        $stmt->bind_param("i", $userId);                          
+        $stmt->execute();
+        
+        $stmt->bind_result($count);
+        $stmt->fetch();
+
+        return $count > 0;
+    }
+
+    function addDBImage($userId, $uuid) {
+        $mysqli = new mysqli($this->config['dbhost'], $this->config['dbuser'], $this->config['dbpass'], $this->config['dbdatabase']);
+        $stmt = $mysqli->prepare("INSERT INTO userimages(userid, url) VALUES (?, ?)");
+        
+        //Make a url here
+        
+
+        $stmt->bind_param("ii", $userId, $groupId);
+        $stmt->execute();
+        
+        $stmt->close();
+        $mysqli->close();
+    }
+
+    function getUserImage($userId) {
+        $mysqli = new mysqli($this->config['dbhost'], $this->config['dbuser'], $this->config['dbpass'], $this->config['dbdatabase']);
+        $stmt = $mysqli->prepare("SELECT userid, url FROM useriamges WHERE userid = ?");
+        $stmt->bind_param("i", $userId);
+        $stmt->execute();
+
+        $stmt->bind_result($userId, $url);
+        $stmt->fetch();
+
+        $result['userid'] = $userId;
+        $result['url'] = $url;
+
+        $stmt->close();
+        $mysqli->close();
+
+        return $result;
+    }
+}
+?>
