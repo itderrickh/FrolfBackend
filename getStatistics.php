928f0d8665433d9f58141b598722144e10cd3064
itderrickh <heined50@uwosh.edu>
2016-12-04 20:08:54 -0600

    Fixed a stats bug.

--- getStatistics.php
+++ getStatistics.php
@@ -1,16 +1,24 @@
 <?php
 require_once './config.php';
 require_once './dbclasses/score.php';
+require_once './dbclasses/user.php';
 require_once './verify.php';
 
 $AJAX_FORM = json_decode(file_get_contents('php://input'), true);
 
 $scoreDAO = new ScoreDAO($config);
+$userDAO = new UserDAO($config);
 $token = $_SERVER['HTTP_AUTHORIZE'];
 
 $userId = $AJAX_FORM['userId'];
 
 if(verifyToken($token, $config)) {
+    if($userId == 0) {
+        $tokenInfo = getTokenInfo($token);
+        $user = $userDAO->getUser($tokenInfo['email']);
+        $userId = $user['id'];
+    }
+    
     $stats = $scoreDAO->getStatistics($userId);
     echo json_encode($stats);
 } else {
